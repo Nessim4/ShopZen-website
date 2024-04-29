c1475af75dcdf654e19c9f4ec66e2b689f714d13
Nessim <88982467+Nessim4@users.noreply.github.com>
2024-04-29 18:09:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Script for navigation bar
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+}
+function toggleForm() {
+    var loginForm = document.getElementById("login-form");
+    var signupForm = document.getElementById("signup-form");
+
+    if (loginForm.style.display === "none") {
+      loginForm.style.display = "block";
+      signupForm.style.display = "none";
+    } else {
+      loginForm.style.display = "none";
+      signupForm.style.display = "block";
+    }
+  }
